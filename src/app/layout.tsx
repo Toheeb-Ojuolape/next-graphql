c387b7675a440ce3db6fcf8c29f3541a5a05857e
Toheeb Ojuolape <45619240+Toheeb-Ojuolape@users.noreply.github.com>
2023-08-28 00:11:31 +0100

    feat: updates for deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,10 @@
-import type { Metadata } from "next";
 import "./globals.css";
-import { useState, useEffect } from "react";
-
-export const metadata: Metadata = {
-  title: "Next Graphql Project",
-  description: "Displays channel lists as a table and graph",
-};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [theme, setTheme] = useState("");
-  useEffect(() => {
-    const storedTheme = localStorage.getItem("theme");
-    if (storedTheme && storedTheme !== null) {
-      setTheme(storedTheme);
-    }
-  }, []);
 
   return <div className={"container"}>{children}</div>;
 }
